e650c4e50c3cd6bf7bd3b1f5f68b16a91fff73b4
ikuma <chemicalbondo@gmail.com>
2022-10-08 14:36:04 +0900

    変数名とdocstringの変更

--- jissa_file.py
+++ jissa_file.py
@@ -5,7 +5,8 @@ import xlwings.constants
 class JissaFile:
     def __init__(self, path):
         """
-        実査数入力対象ファイルを開く
+        実査数入力対象ファイルを開く。
+        有償・無償でPI-Netの画面を切り替えるためファイル名でis_paidを設定する。
         :param path: 対象エクセルファイルのパス
         """
         self.wb = xw.Book(path)
@@ -20,9 +21,9 @@ class JissaFile:
         """
         try:
             found = self.ws.api.Columns("A").Find(What=zuban, LookAt=xw.constants.LookAt.xlWhole)
-            jissa_su = self.ws.range(f"I{found.Row}")
-            jissa_su.select()
-            jissa_su.color = 255, 255, 0
-            return jissa_su.value
+            inventory_quantiry_cell = self.ws.range(f"I{found.Row}")
+            inventory_quantiry_cell.select()
+            inventory_quantiry_cell.color = 255, 255, 0
+            return inventory_quantiry_cell.value
         except AttributeError:
             print(f"{zuban} is not found.")
